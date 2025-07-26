58051e4c148acbbd179e34faee95fc4031eb6d1f
abhishek basnal <86596432+abhishek-bansal25@users.noreply.github.com>
2025-07-26 10:41:37 +0000

    migrated to vite

--- react-web-portfolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white py-4 text-center">
-      <p>&copy; 2025 Abhishek Bansal. All rights reserved.</p>
-    </footer>
-  );
-}
